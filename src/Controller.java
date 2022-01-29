a562eacecca63ff156b7c00f5a73476e5a813cbf
IAFNN <96941212+IAFNN@users.noreply.github.com>
2022-01-29 11:12:30 +0200

    Dichotomy method

--- src/Controller.java
+++ src/Controller.java
@@ -1,6 +1,7 @@
 public class Controller {
     public static double CalculateDichotomyMethod(double a, double b, double eps, Function<Double> function){
         double c = (a + b) / 2;
+        System.out.println("a:" + a + "\tb:" + b + "\tc:" + c);
         if(Math.abs(function.calculateFunction(c)) < eps){
             return c;
         }else if(function.calculateFunction(c) * function.calculateFunction(a) < 0){

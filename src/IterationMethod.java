5589ce9f39391c2f3d5a50ec6cec61f8f110f3b7
IAFNN <96941212+IAFNN@users.noreply.github.com>
2022-02-05 22:32:34 +0200

    Console logging and extreme cases handling

--- src/IterationMethod.java
+++ src/IterationMethod.java
@@ -12,11 +12,17 @@ public class IterationMethod extends Method{
         results.add(new ArrayList<>(Collections.nCopies(system.size(), 0.0)));
         results.add(new ArrayList<>(Collections.nCopies(system.size(), 0.0)));
         double difference = Double.POSITIVE_INFINITY;
+        int count = 0;
         while (difference > eps) {
             for (int i = 0; i < system.size(); i++) {
                 results.get(i).set(0, system.get(i).apply(results.get(0).get(0), results.get(1).get(0)));
             }
             difference = checkDifference(results);
+            System.out.println("x: " + results.get(0).get(0) + "\t\ty: " + results.get(1).get(0));
+            count++;
+            if(count > 100){
+                throw new RuntimeException("Error, iteration method isn't convergent");
+            }
         }
         return results;
     }
--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args){
+        System.out.println("Iteration method:");
         IterationMethod iterationMethod = new IterationMethod(Arrays.asList((aDouble, aDouble2) -> 1.5 - Math.cos(aDouble2),
                 (aDouble, aDouble2) -> (1 + Math.sin(aDouble - 0.5)) / 2), 0.000001,
                 (aDouble, aDouble2) -> Math.cos(aDouble2) + aDouble, 1.5);
